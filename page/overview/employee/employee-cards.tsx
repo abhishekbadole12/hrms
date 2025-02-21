722c3a9d6e4fb977d76d2d5e2a96c18d797e12c6
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-02-21 16:17:25 +0530

    employee cards

--- page/overview/employee/employee-cards.tsx
+++ page/overview/employee/employee-cards.tsx
@@ -1,12 +1,12 @@
-import BreadCrumb from '@/components/bread-crumb/bread-crumb'
-import EmployeeCardsComponent from '@/components/employee/view/employee-cards'
-import React from 'react'
+import BreadCrumb from "@/components/bread-crumb/bread-crumb";
+import EmployeeCardsComponent from "@/components/employee/cards/employee-cards";
+import React from "react";
 
 export default function EmployeeCardsPage() {
   return (
     <div>
-        <BreadCrumb title='Employee cards' />
-        <EmployeeCardsComponent />
+      <BreadCrumb title="Employee cards" />
+      <EmployeeCardsComponent />
     </div>
-  )
+  );
 }
