0d3731f0f3a2a2104c4fe908632532112e62f8e6
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-04-02 12:44:57 +0530

    improve popover-item component

--- src/components/modal/popover-item.tsx
+++ src/components/modal/popover-item.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 interface PopoverItemProps {
   children: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function PopoverItem({ children }: PopoverItemProps) {
+export default function PopoverItem({ children, onClick }: PopoverItemProps) {
   return (
-    <div className="py-2 px-3 text-sm text-primary rounded-md cursor-pointer hover:bg-gray-100 hover:font-medium">
-      <p className="">{children}</p>
-    </div>
+    <button
+      type="submit"
+      className="w-full text-start py-2 px-3 text-sm text-primary rounded-md hover:bg-gray-100 hover:font-medium"
+      onClick={onClick}
+    >
+      <p>{children}</p>
+    </button>
   );
 }
