a32a49b3a5c7b1b8b1465afebb9b1e2db6da6f8d
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-04-03 16:20:30 +0530

    register user schema check zod updated

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -3,16 +3,20 @@ import { z } from "zod";
 // User registration Zod schema
 export const RegisterUserSchema = z.object({
   first_name: z.string().min(1, "First name is required"),
+  middle_name: z.string().min(1, "Middle name is required"),
   last_name: z.string().min(1, "Last name is required"),
   gender: z.enum(["MALE", "FEMALE", "OTHER"], {
     errorMap: () => ({ message: "Invalid gender" }),
   }),
-  phone_number: z.string().regex(/^\+?[1-9]\d{1,14}$/, "Invalid phone number"), // You can adjust the regex as per your needs
+  country_code: z.string().regex(/^\+\d{1,3}$/, "Invalid country code"),
+  phone_number: z
+    .string()
+    .regex(/^\d{10}$/, "Phone number must be exactly 10 digits"),
   email: z.string().email("Invalid email format"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
   user_role: z.enum(["ADMIN", "MANAGER", "EMPLOYEE", "HR"], {
     errorMap: () => ({ message: "User role is required" }),
   }),
+  status: z.enum(["ACTIVE", "INACTIVE"]),
 });
 
 // Login user Zod schema
@@ -21,8 +25,8 @@ export const LoginUserSchema = z.object({
   password: z.string().min(1, "Password is required"),
 });
 
-export type SessionPayload = {
-  user_id: string | number;
+export type ISessionPayload = {
+  user_id: string;
   expiresAt: Date;
 };
 
