ed0a9e64bf192f0c7a8a0694572e4208c4af6dc4
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-04-05 19:20:00 +0530

    fix: util date function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,11 @@
 // Function to get the next date
 export const getNextDate = (date: string) => {
-  if (!date) {
-    return "";
-  }
+  if (!date) return "";
 
   const currentDate = new Date(date);
   const nextDate = new Date(currentDate);
   nextDate.setDate(currentDate.getDate() + 1);
+
+  // Format the date to YYYY-MM-DD
+  return nextDate.toISOString().split("T")[0];
 };
