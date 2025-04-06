653acd2433d1d26c76e84d8bbeadf4d2b8b07d09
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-04-06 14:40:36 +0530

    loading component - size

--- src/components/common/snakebar/loading.tsx
+++ src/components/common/snakebar/loading.tsx
@@ -1,23 +1,41 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
 import "./loading.css";
 
-export default function Loading() {
+interface LoadingProps {
+  size: "sm" | "lg";
+}
+
+export default function Loading({ size }: LoadingProps) {
   const [loading, setLoading] = useState(true);
 
+  const containerSize = size === "sm" ? "w-1/3" : "";
+
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 5000);
+    const timer = setTimeout(() => setLoading(false), 500);
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="relative w-full p-4 rounded-md">
-      <div className="relative w-full h-2 bg-gray-200 overflow-hidden rounded-md mb-2">
+    <div
+      className={clsx(
+        "flex flex-col items-center justify-center",
+        containerSize
+      )}
+    >
+      <div
+        className={clsx(
+          "relative w-full h-2 bg-gray-200 overflow-hidden rounded-md mb-2"
+        )}
+      >
         <div className="loader-bar"></div>
       </div>
 
-      <p className="text-sm font-medium text-gray-700 text-center">Loading...</p>
+      <p className="text-sm font-medium text-gray-700 text-center">
+        Loading...
+      </p>
     </div>
   );
 }
