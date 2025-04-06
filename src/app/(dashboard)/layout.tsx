b7238059761828501ccf99c1a05b1b3e8a297993
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-04-06 14:50:49 +0530

    fix: DashboardLayout css fix

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,14 +3,14 @@ import PrivateHeader from "@/components/header/private-header";
 import { Sidebar } from "@/components/sidebar/sidebar";
 import clsx from "clsx";
 
-export default function Authlayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <div
-      className={clsx("h-full flex p-4 pr-0", {
+      className={clsx("h-screen flex p-4 pr-0", {
         "bg-[#f9f9f9]": true,
       })}
     >
@@ -19,7 +19,7 @@ export default function Authlayout({
       <section className="w-full h-full py-2 px-12 flex flex-col min-h-0">
         <PrivateHeader />
 
-        <div className="px-10">{children}</div>
+        <div className="px-10 h-full">{children}</div>
       </section>
     </div>
   );
